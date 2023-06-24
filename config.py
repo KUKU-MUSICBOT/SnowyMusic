733bc43e337cb05e12c25bcc2685302c9a0c8f13
AN3H <136014659+KUKU-MUSICBOT@users.noreply.github.com>
2023-06-25 04:22:58 +0530

    Update config.py
    Signed-off-by: AN3H  <136014659+KUKU-MUSICBOT@users.noreply.github.com>

--- config.py
+++ config.py
@@ -14,19 +14,18 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "ᴀɴᴏɴ ダ ᴍᴜsɪᴄ")
-
-OWNER_ID = list(map(int, getenv("OWNER_ID", "5901389360").split()))
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "[ʀᴜʜɪ xʙᴏᴛ]")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "1952625698").split()))
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/ZexxySnowy/SnowyMusic")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/KUKU-MUSICBOT/RuhiXMusic")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/SNOWY_UPDATES")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/SNOWY_SUPPORT")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/WTF_FCC")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/XD_CUTETY")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
@@ -76,34 +75,37 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/8ea607b00a81fd8e7bb61.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/9aac80456d78f27247885.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
-    "https://te.legra.ph/file/35119eb0878cfe8669b04.jpg",
+    "https://te.legra.ph/file/f60a54b51b32906df76a3.jpg",
 )
 
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
+PLAYLIST_IMG_URL = "https://te.legra.ph/file/9aac80456d78f27247885.jpg"
 
-GLOBAL_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
+GLOBAL_IMG_URL = "https://te.legra.ph/file/9aac80456d78f27247885.jpg"
 
-STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
+STATS_IMG_URL = "https://te.legra.ph/file/f60a54b51b32906df76a3.jpg"
 
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
+TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/9aac80456d78f27247885.jpg"
 
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
+TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/f60a54b51b32906df76a3.jpg"
 
-STREAM_IMG_URL = "https://te.legra.ph/file/54ab2a136ad09de318c5f.jpg"
 
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
 
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
+STREAM_IMG_URL = "https://te.legra.ph/file/9aac80456d78f27247885.jpg"
 
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
+SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/f60a54b51b32906df76a3.jpg"
 
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
+YOUTUBE_IMG_URL = "https://te.legra.ph/file/9aac80456d78f27247885.jpg"
 
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/f60a54b51b32906df76a3.jpg"
+
+SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/9aac80456d78f27247885.jpg"
+
+
+SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/f60a54b51b32906df76a3.jpg"
 
 
 def time_to_seconds(time):
@@ -130,9 +132,9 @@ if UPSTREAM_REPO:
 if PING_IMG_URL:
     if PING_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", PING_IMG_URL):
-            PING_IMG_URL = "https://te.legra.ph/file/35119eb0878cfe8669b04.jpg"
+            PING_IMG_URL = "https://te.legra.ph/file/f60a54b51b32906df76a3.jpg"
 
 if START_IMG_URL:
     if START_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", START_IMG_URL):
-            START_IMG_URL = "https://te.legra.ph/file/8ea607b00a81fd8e7bb61.jpg"
+            START_IMG_URL = "https://te.legra.ph/file/9aac80456d78f27247885.jpg"
